74c1fd01d1f8781d2024a32a2376fd087347957c
mattp0irier <poiriermatthewj@gmail.com>
2022-02-24 23:20:00 -0600

    more work on interpreter

--- interpreter.cpp
+++ interpreter.cpp
@@ -8,15 +8,183 @@ using namespace std;
 
 class Interpreter {
     private:
+        // SEXP applyUserFun(String nm, VALUELIST actuals) {
+        //     FUNDEF fun = fetchFun(nm);
+        //     if (fun == null) {
+        //         ERROR("Undefined function: " + nm);
+        //         return NIL;
+        //     }
+        //     if (lengthNL(fun.formals) != lengthVL(actuals)) {
+        //         ERROR("Wrong number of arguments to: " + nm);
+        //         return NIL;
+        //     }
+        //     ENV rho = new ENV(fun.formals, actuals);
+        //     return eval(fun.body, rho);
+        // }
 
+        VALUELIST evalList(EXPLIST el, ENV rho) {
+            if (el == null)
+                return null;
+            SEXP h = eval(el.head, rho);
+            VALUELIST t = evalList(el.tail, rho);
+            return new VALUELIST(h, t);
+        }
 
-    public: 
+        SEXP applyCtrlOp(Token controlOP, EXPLIST args, ENV rho) {
+            SEXP s = NIL;
+            switch (controlOP.text) {
+                case "IF":
+                    if (isTrueVal(eval(args.head, rho)))
+                        return eval(args.tail.head, rho);
+                    else
+                        return eval(args.tail.tail.head, rho);
+                    break;
+                case "WHILE":
+                    s = eval(args.head, rho);
+                    while (s != NIL) {
+                        s = eval(args.tail.head, rho);
+                        s = eval(args.head, rho);
+                    }
+                    return s;
+                    break;
+                case "SET":
+                    s = eval(args.tail.head, rho);
+                    String varble = ((VAREXP)(args.head)).varble;
+                    if (isBound(varble, rho))
+                        assign(varble, s, rho);
+                    else if (isBound(varble, globalEnv))
+                        assign(varble, s, globalEnv);
+                    else
+                        bindVar(varble, s, globalEnv);
+                    return s;
+                    break;
+                case "BEGIN": 
+                    while (args.tail != null) {
+                        s = eval(args.head, rho);
+                        args = args.tail;
+                    }
+                    // very strange this
+                    s = eval(args.head, rho);
+                    return s;
+                    break;
+            }
+            return s;
+        }
+
+        NUMSXP applyArithOp(Token op, int n1, int n2) {
+            int result = 0;
+            result = switch (op.text) {
+            case ("+") -> n1 + n2; case ("-") -> n1 - n2; case ("*") -> n1 * n2; case ("/") -> n1 / n2; case ("%") -> n1 % n2; default -> 0;
+            };
+            return new NUMSXP(result); }
+            SEXP applyRelOp(Token op, int n1, int n2) { boolean result;
+            result = switch (op.text) {
+            case ("<") -> n1 < n2; case (">") -> n1 > n2; case ("<=") -> n1 <= n2; case (">=") -> n1 >= n2; case ("=") -> n1 == n2; default -> false;
+            };
+            if (result)
+            return TRUE; else
+            return NIL;
+        }
+
+        SEXP applyValueOp(Token op, VALUELIST vl) {
+            SEXP result = NIL;
+            SEXP s1, s2 = NIL;
+            if (op.arity != 0 && op.arity != lengthVL(vl)) {
+                ERROR("Wrong number of arguments to " + op.text + " expected " + op.arity + " but found " + lengthVL(vl));
+                return NIL; }
+s1 = vl.head; // 1st actual if (op.arity == 2)
+s2 = vl.tail.head; // 2nd actual
+if (op.optype == Token.ARITHMATIC || op.optype == Token.RELATIONAL) {
+if (s1.type == "Number" && s2.type == "Number") { NUMSXP n1 = (NUMSXP) s1;
+NUMSXP n2 = (NUMSXP) s2;
+if (op.optype == Token.ARITHMATIC)
+result = applyArithOp(op, n1.intval, n2.intval);
+else
+result = applyRelOp(op, n1.intval, n2.intval); } else
+ERROR("Non-arithmatic arguments to " + op.text); } else if (op.arity == 2)
+result = apply(op, s1, s2);
+else
+result = apply(op, s1); return result;
+}
+
+        SEXP apply(Token op, SEXP s1, SEXP s2) { SEXP result = NIL;
+switch (op.text) {
+case ("CONS") -> result = new LISTSXP(s1, s2); case ("EQ?"), ("=") -> {
+if (s1 == NIL && s2 == NIL) { result = TRUE;
+} else if (s1.type == "Number" && s2.type == "Number") { NUMSXP n1 = (NUMSXP) s1;
+NUMSXP n2 = (NUMSXP) s2;
+if (n1.intval == n2.intval)
+result = TRUE;
+} else if (s1.type == "Symbol" && s2.type == "Symbol") {
+SYMSXP n1 = (SYMSXP) s1; SYMSXP n2 = (SYMSXP) s2; if (n1.symval == n2.symval)
+result = TRUE; }
+} }
+return result; }
+
+SEXP apply(Token op, SEXP s1) { SEXP result = NIL;
+switch (op.text) {
+case ("NOT") -> { if (s1 == NIL)
+result = TRUE; }
+case ("CAR") -> {
+if (s1.type == "List") {
+LISTSXP concell = (LISTSXP) s1;
+result = concell.carval; } else
+ERROR("car applied to non-list"); }
+case ("CDR") -> {
+if (s1.type == "List") {
+LISTSXP concell = (LISTSXP) s1;
+result = concell.cdrval; } else
+ERROR("cdr applied to non-list"); }
+case ("NIL?"), ("NULL?") -> { if (s1 == NIL)
+result = TRUE; }
+case ("NUMBER?") -> {
+if (s1.type == "Number")
+result = TRUE; }
+case ("SYMBOL?") -> {
+if (s1.type == "Symbol")
+result = TRUE; }
+case ("LIST?") -> {
+if (s1.type == "List")
+result = TRUE; }
+case ("PRINT") -> { System.out.println(s1.toString());
+result = s1; }
+}
+return result; }
+
+    public:
         Interpreter() {
 
         }
 
         // unsure about void type
-        void evaluate(EXP expression) {
-
+        void eval(EXP expression) {
+            Token op;
+            if (e instanceof VALEXP s) {
+                return s.sxp;
+            }
+            else if (e instanceof VAREXP v) {
+                if (isBound(v.varble, rho)) {
+                    return fetch(v.varble, rho);
+                }
+                if (isBound(v.varble, globalEnv)) {
+                    return fetch(v.varble, globalEnv);
+                }
+                ERROR("Undefined variable " + v.varble);
+            }
+            else if (e instanceof APEXP a) {
+                op = Token.builtin(a.optr);
+                if (op == null) {
+                    return applyUserFun(a.optr, evalList(a.args, rho));
+                }
+                else {
+                    if (op.optype == Token.CONTROL) {
+                        return applyCtrlOp(op, a.args, rho);
+                    }
+                    else {
+                        return applyValueOp(op, evalList(a.args, rho));
+                    }
+                }
+            }
+            return NIL;
         }
 };
