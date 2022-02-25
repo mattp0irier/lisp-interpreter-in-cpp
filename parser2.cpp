3e68aec90856604c878daaafbeaaf0cfd1a67a5c
mattp0irier <poiriermatthewj@gmail.com>
2022-02-24 22:15:37 -0600

    more fixes to parsing class

--- parser2.cpp
+++ parser2.cpp
@@ -24,70 +24,131 @@ class Parser {
             // needed to broaden the definition of name to include builtins
             if (tokenList[pos].getType() == INTEGER || tokenList[pos].getType() == FLOAT) {
                 // ERROR("Expected name, instead read :", userinput(pos, "parseName"));
-                // return "error";
-                string name = tokenList[pos].getVal();
+                return "error";
+            }
+            string name = tokenList[pos].getVal();
+            pos++;
+            // install(name);
+            return name;
+        }
+
+        NAMELIST parseNL() {
+            string nm;
+            NAMELIST nl;
+            if (tokenList[pos].getType() == RIGHT_PAREN) {
                 pos++;
-                // install(name);
-                return name;
+                return null;
             }
+            else {
+                nm = parseName();
+                nl = parseNL();
+                return new NAMELIST(nm, nl);
+            }
+        }
 
-            NAMELIST parseNL() {
-                string nm;
-                NAMELIST nl;
-                if (tokenList[pos].getType() == RIGHT_PAREN) {
+        EXP parseExp() {
+            string nm;
+            EXPLIST el;
+            if (tokenList[pos].getType() == LEFT_PAREN) { // APEXP
+                pos++;
+                if (tokenList[pos].getType() == RIGHT_PAREN) { // NIL
                     pos++;
-                    return null;
+                    S_EXP nil = S_EXP("(");
+                    return VALEXP(nil);
                 }
-                else {
-                    nm = parseName();
-                    nl = parseNL();
-                    return new NAMELIST(nm, nl);
-                }
+                nm = parseName();
+                el = parseEL();
+                return APEXP(nm, el);
             }
+            else if (tokenList[pos].getType() == INTEGER || tokenList[pos].getType() == FLOAT || tokenList[pos].getType() == T || tokenList[pos].getType() == LEFT_PAREN || tokenList[pos].getType() == RIGHT_PAREN)
+                return VALEXP(parseVal());
+            else
+                return VAREXP(parseName());
+        }
 
-            EXP parseExp() {
-                string nm;
-                EXPLIST el;
-                if (tokenList[pos].getType() == LEFT_PAREN) { // APEXP
-                    pos++;
-                    if (tokenList[pos].getType() == RIGHT_PAREN) { // NIL
-                        pos++;
-                        return new VALEXP(NIL);
-                    }
-                    nm = parseName();
-                    el = parseEL();
-                    return new APEXP(nm, el);
-                }
-                else if (isValue(pos, "parseExp"))
-                    return VALEXP(parseVal());
-                else
-                    return VAREXP(parseName());
+        EXPLIST parseEL() {
+            if (tokenList[pos].getType() == END_OF_FILE) {
+                // ERROR("Expected ) found EOF");
+                return null;
             }
+            if (tokenList[pos].getType() == RIGHT_PAREN) {
+                pos++;
+                return null;
+            }
+            else {
+                EXP e = parseExp();
+                EXPLIST el = parseEL();
+                return EXPLIST(e, el);
+            }
+        }
 
-            EXPLIST parseEL() {
-                if (tokenList[pos].getType() == END_OF_FILE) {
-                    ERROR("Expected ) found EOF");
-                    return null;
-                }
-                if (tokenList[pos].getType() == RIGHT_PAREN) {
-                    pos++;
-                    return null;
-                }
-                else {
-                    EXP e = parseExp();
-                    EXPLIST el = parseEL();
-                    return new EXPLIST(e, el);
-                }
+        SEXP parseList() {
+            SEXP car, cdr;
+            if (tokenList[pos].getType() == RIGHT_PAREN) {
+                pos++;
+                return NIL;
+                // return mkSExp(NILSXP);
             }
+            else {
+                car = parseSExp();
+                cdr = parseList();
+                return new LISTSXP(car, cdr);
+            }
+        }
 
-        public:
-            Parser(vector<Token> tokenList) {
-                this->tokenList = tokenList;
-                int pos = 0;
+        SEXP parseInt() {
+            int value;
+            if (tokenList[pos].getType() == MINUS && tokenList[pos+1].getType() == INTEGER) {
+                value = -1 * tokenList[pos+1].getIntVal();
+                pos += 2;
             }
+            else if (tokenList[pos].getType() == MINUS && tokenList[pos+1].getType() == FLOAT) {
+                value = -1 * tokenList[pos+1].getFloatVal();
+                pos += 2;
+            }
+            else if (tokenList[pos].getType() == FLOAT) {
+                value = tokenList[pos].getFloatVal();
+                pos += 1;
+            }
+            else {
+                value = tokenList[pos].getIntVal();
+                pos += 1;
+            }
+            return new NUMSXP(value);
+        }
 
-            EXP getNextExpression() {
-                // do things
+        SEXP parseSym() {
+            string symbolname = parseName();
+            return new SYMSXP(symbolname);
+        }
+
+        SEXP parseSExp() {
+            if (tokenList[pos].getType() == T) {
+                pos++;
+                return TRUE;
             }
+            if (tokenList[pos].getType() == LEFT_PAREN) {
+                pos++;
+                return parseList();
+            }
+            if (tokenList[pos].getType() == INTEGER || tokenList[pos].getType() == FLOAT)
+                return parseInt();
+            return parseSym();
+        }
+        SEXP parseVal() {
+            // if (userinput(pos, "parseSExp") == Token.TIC)
+            //     pos++;
+            return parseSExp();
+        }
+
+    public:
+        Parser(vector<Token> tokenList) {
+            this->tokenList = tokenList;
+            int pos = 0;
+        }
+
+        EXP getNextExpression() {
+            // do things
+        }
 
 };
