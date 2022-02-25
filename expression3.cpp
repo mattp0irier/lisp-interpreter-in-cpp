6f144d162d66f9a4c3a5478435bd05495485d00a
mattp0irier <poiriermatthewj@gmail.com>
2022-02-24 20:53:28 -0600

    expression class

--- expression3.cpp
+++ expression3.cpp
@@ -1,3 +1,42 @@
 //trevor
 
 //matt
+
+class EXP {
+};
+
+class VALEXP: public EXP {
+    SEXP sxp;
+    VALEXP(SEXP sxp) {
+        this->sxp = sxp;
+    }
+};
+
+class VAREXP: public EXP {
+    string varble;
+    VAREXP(string variable) {
+        this->varble = variable;
+    }
+};
+
+class APEXP: public EXP {
+    private:
+        string optr;
+        EXPLIST args;
+    public:
+        APEXP(string optr, EXPLIST args) {
+            this->optr = optr;
+            this->args = args;
+        }
+};
+
+class EXPLIST {
+    private:
+        EXP head;
+        EXPLIST *tail;
+    public:
+        EXPLIST(EXP head, EXPLIST *tail) {
+            this->head = head;
+            this->tail = tail;
+        }
+};
