11f274ffabc529744ff2d00495b77048066a5941
mattp0irier <poiriermatthewj@gmail.com>
2022-02-27 23:20:13 -0600

    added comments to scanner

--- scanner.hpp
+++ scanner.hpp
@@ -8,14 +8,16 @@
 
 using namespace std;
 
+// Scanner class: used to create tokens from input text
 class Scanner {
     private:
         string input;
         int startIndex;
         int index = 0;
         vector<Token> tokens;
-        int currentLine;
+        int currentLine; // keeps track of line number being scanned
 
+        // map for keyword matching to TokenType
         map<string, TokenType> keywords = {
             {"if", IF},
             {"while", WHILE},
@@ -33,6 +35,7 @@ class Scanner {
             {"call", CALL}
         };
 
+        // map for TokenType matching to number of expected args
         map<TokenType, int> arityMap = {
             {IF, 3},
             {WHILE, 2},
@@ -50,38 +53,43 @@ class Scanner {
             {IDENTIFIER, 0}
         };
 
+        // getNextChar: increment index and get character at that index
         char getNextChar() {
             return input.at(index++);
         }
 
+        // peek: get character at index if not at end of length
         char peek() {
             if (done()) return '\0';
             return input.at(index);
         }
 
+        // peekNext: get next character
         char peekNext() {
             return input.at(index + 1);
         }
 
+        // done: check for out of bounds on string
         bool done() {
             return index >= input.length();
         }
 
+        // addToken: add token with string value to TokenList
         void addToken(TokenType type, string value, int arity) {
             tokens.push_back(Token(type, value, currentLine, arity));
-            // cout << "added string token" << endl;
         }
 
+        // addToken: add token with int value to TokenList
         void addToken(TokenType type, int value, int arity) {
-            // cout << "adding token 2" << endl;
             tokens.push_back(Token(type, value, currentLine, arity));
-            // cout <<"pushback complete"<<endl;
         }
 
+        // addToken: add token with double value to TokenList
         void addToken(TokenType type, double value, int arity) {
             tokens.push_back(Token(type, value, currentLine, arity));
         }
 
+        // scanToken: get next character and create token
         void scanToken() {
             char c = getNextChar();
             switch (c) {
@@ -95,6 +103,7 @@ class Scanner {
                 addToken(PLUS, string(1, c), 2);
                 break;
             case '-':
+                // check for negative number
                 if (isdigit(peek())){ 
                     getNumber();
                     break;
@@ -114,36 +123,39 @@ class Scanner {
                 addToken(EQUAL, string(1, c), 2);
                 break;
             case '<':
+                // check for <=
                 if (peek() == '=') {addToken(LTE, string(2, c), 2); index++;}
                 else addToken(LESS_THAN, string(1, c), 2);
                 break;
             case '>':
+                // check for >=
                 if (peek() == '=') {addToken(GTE, string(2, c), 2); index++;}
                 else addToken(GREATER_THAN, string(1, c), 2);
                 break;
             case 't':
             case 'T':
+                // check to ensure not a variable or function name
                 if (isalpha(peek())) getIdentifier();
                 else addToken(T, string(1, c), 0);
                 break;
             // ignore whitespace
             case ' ':
-                break;
             case '\r':
-                break;
             case '\t':
                 break;
             case '\n':
                 currentLine++; // increment line counter
                 break;
-            // string handler
+            // string literal handler
             case '\"':
                 getString();
                 break;
             default:
+                // check for number
                 if(isdigit(c)) {
                     getNumber();
                 }
+                // check for identifier
                 else if (isalpha(c)) {
                     getIdentifier();
                 }
@@ -154,15 +166,19 @@ class Scanner {
             }
         }
 
+        // getNumber: get int or float from input
         void getNumber() {
+            // check for negative number
             bool neg = false;
             if (peek() == '-') { 
                 neg = true;
                 getNextChar();
             }
+            // continue while still digits following the position
             while (isdigit(peek())) {
                 getNextChar();
             }
+            // check for floats
             if (peek() == '.' && isdigit(peekNext())) {
                 getNextChar();
                 while (isdigit(peek())) getNextChar();
@@ -175,7 +191,9 @@ class Scanner {
             }
         }
 
+        // getIdentifier: get variable or function identifier from input
         void getIdentifier() {
+            // continue while still characters following the current position
             while (isalpha(peek())) {
                 getNextChar();
             }
@@ -183,56 +201,64 @@ class Scanner {
             // Identifier may end with ?
             if (peek() == '?') getNextChar();
 
+            // convert identifier to lower case
             string idName = input.substr(startIndex, index-startIndex);
             for (int i=0; i<idName.length(); i++){
                 idName[i] = tolower(idName[i]);
             }
+            
             TokenType type;
+            // check for builtin keyword using map
             if (keywords.count(idName)){
                 type = keywords[idName];
             }
+            // otherwise identifier
             else {
                 type = IDENTIFIER;
             } 
             addToken(type, idName, arityMap[type]);
         }
 
+        // getString: get string literal from input
         void getString() {
-            //cout << "getString" << endl;
+            // while not end of input and not another quote, get next char
             while (peek() != '\"' && !done()) {
                 if (peek() == '\n') currentLine++;
                 getNextChar();
             }
-            //cout << "while loop broke at index " << index << endl;
             if (done()) {
                 cout << "Error: Unterminated String" << endl;
                 return;
             }
-            getNextChar();
+            getNextChar(); // increment position
             addToken(STRING, input.substr(startIndex+1, index-startIndex-2), 0);
         }
 
 
     public:
+        // Scanner: constructs scanner with input provided as string
         Scanner(string input) {
             this->input = input;
             currentLine = 0;
         }
 
+        // scanTokens: get tokens from input and return token list
         vector<Token> scanTokens() {
-            // cout << "beginning scan" << endl;
+            // while not end of input
             while(index < input.length()) {
                 startIndex = index;
-                //cout << "scanning token at index " << index << endl;
                 scanToken();
             }
 
-            addToken(END_OF_FILE, -1, -1);
+            addToken(END_OF_FILE, -1, -1); // add EOF token when input complete
             return tokens;
         }
 
+        // printTokens: output token list
         void printTokens() {
+            // loop through tokens
             for (int i=0; i<tokens.size(); i++){
+                // print correct value based on type
                 if(tokens[i].getType() == INTEGER) {
                     cout << "\"" << tokens[i].getIntVal() << "\"";
                 }
